a3c78d7483ef5345a89b80e4d968ab94363ae366
Ross Westwater <ross.westwater@gmail.com>
2021-11-18 19:06:05 -0700

    added quiz question generator, score saving feature and clearing features. can style at will.

--- assets/script.js
+++ assets/script.js
@@ -3,82 +3,156 @@ var countdownDisplay = document.getElementById("counter-display");
 var qstnDisplay = document.getElementById("question-display");
 var selectDisplay = document.getElementById("selection-display");
 var answerDisplay = document.getElementById("answer-display");
+var goodLuck = document.getElementById("good-luck");
 
 timer = 60;
 points = 0;
 indexValue = 0;
+myScore = {
+  initials: [],
+  score: [],
+};
 
 var questionObj = [
   {
-  question: "Who is the most powerful villain?",
-  selection: ["Darth Vader", "King Shark", "Thanos", "Hannibal Lecter"],
-  answer: "Thanos"
+    question: "Who is the most powerful villain?",
+    selection: ["Darth Vader", "King Shark", "Thanos", "Hannibal Lecter"],
+    answer: "Thanos",
   },
   {
-  question: "Which of the following is my dog?",
-  selection: ["Sparkles", "Elmo", "Dribbles", "Speedbump"],
-  answer: "Elmo"
+    question: "Which of the following is my dog?",
+    selection: ["Sparkles", "Elmo", "Dribbles", "Speedbump"],
+    answer: "Elmo",
   },
   {
-  question: "What is my favorite food?",
-  selection: ["Pizza", "Ice Cream", "Asian Pears", "Swedish Dumplings"],
-  answer: "Pizza"
+    question: "What is my favorite food?",
+    selection: ["Pizza", "Ice Cream", "Asian Pears", "Swedish Dumplings"],
+    answer: "Pizza",
   },
   {
-  question: "How do you open a jar lid?",
-  selection: ["Twist it", "Pull it", "Break it", "Bop it"],
-  answer: "Twist it"
+    question: "How do you open a jar lid?",
+    selection: ["Twist it", "Pull it", "Break it", "Bop it"],
+    answer: "Twist it",
   },
   {
-  question: "Which of the following is NOT a TA or Instructor of this course?",
-  selection: ["Mike", "Karina", "Anthony", "David"],
-  answer: "David"
-}];
+    question:
+      "Which of the following is NOT a TA or Instructor of this course?",
+    selection: ["Mike", "Karina", "Anthony", "David"],
+    answer: "David",
+  },
+];
 
-
-function chooseQstn() {
-  if(timer === 0 || indexValue === questionObj.length) {
-    qstnDisplay.style.display = "none";
-    // clear all the stuff on the page to allow the score information to pop up
-  }
-  var generateQstn = questionObj[indexValue];
-  console.log(questionObj[indexValue]);
-  qstnDisplay.textContent = questionObj.question;
-  console.log(generateQstn);
-  for (var i = 0; i < questionObj.question.length; i++){
-    var element = questionObj.selection[i];
-    console.log(element);
-    var chsBtn = document.createElement("button");
-    chsBtn.textContent = element;
-    chsBtn.setAttribute("value", element);
-    chsBtn.onclick=checkQuestion;
-    selectDisplay.appendChild(chsBtn);
-    indexValue++;
-  }
-};
-
-
-var Countdown = function(){
-
-  var CountdownDetails = setInterval(function() {
-    if (timer > 0) {
+var Countdown = function () {
+  var CountdownDetails = setInterval(function () {
+    if (timer > 0 && indexValue === questionObj.length) {
+      clearInterval(CountdownDetails);
+      countdownDisplay.textContent = "Game Over! Let's see how you did!";
+      endGame();
+    } else if (timer > 0) {
       countdownDisplay.textContent = timer + " seconds remaining!";
       timer--;
       console.log(timer + " seconds remaining");
-    }
-    else if(timer === 0) {
+    } else if (timer <= 0) {
       clearInterval(CountdownDetails);
-      countdownDisplay.textContent = "Game Over, Loser!"
-      // endgame();
+      countdownDisplay.textContent = "Game Over, Loser! Let's see how you did!";
+      endGame();
+
     }
-    }, 1000);
+  }, 1000);
 };
 
-btnStart.addEventListener('click', function(){
-  document.getElementById("preGame").hidden = true;
-  document.getElementById("postGame").hidden = false;
-  document.getElementById("start").hidden = true;
-  // Countdown();
-  chooseQstn();
-}, false);
+btnStart.addEventListener(
+  "click",
+  function () {
+    document.getElementById("preGame").hidden = true;
+    document.getElementById("postGame").hidden = false;
+    document.getElementById("start").hidden = true;
+    Countdown();
+    chooseQstn();
+  },
+  false
+);
 
+function clearHTML() {
+  goodLuck.innerHTML = "";
+  qstnDisplay.innerHTML = "";
+  selectDisplay.innerHTML = "";
+  answerDisplay.innerHTML = "";
+}
+
+function chooseQstn() {
+  if (indexValue === questionObj.length) {
+    clearHTML();
+    countdownDisplay.textContent = "Game Over! Let's see how you did!";
+    return;
+  }
+  var generateQstn = questionObj[indexValue];
+  qstnDisplay.innerHTML = "";
+  selectDisplay.innerHTML = "";
+  setTimeout(() => {
+    answerDisplay.textContent = "";
+  }, 1500);
+  qstnDisplay.textContent = generateQstn.question;
+  for (var i = 0; i < generateQstn.selection.length; i++) {
+    var element = generateQstn.selection[i];
+    var chsBtn = document.createElement("button");
+    chsBtn.textContent = element;
+    chsBtn.addEventListener("click", confirmAnswer);
+    selectDisplay.appendChild(chsBtn);
+  }
+}
+
+var confirmAnswer = function (event) {
+  var target = event.target;
+  if (target.innerText === questionObj[indexValue].answer) {
+    points += 10;
+    answerDisplay.textContent = "Correct!";
+    console.log(answerDisplay.textContent);
+  } else {
+    timer -= 15;
+    answerDisplay.textContent = "Incorrect!";
+    console.log(answerDisplay.textContent);
+  }
+  indexValue++;
+  chooseQstn();
+};
+
+var endGame = function () {
+  console.log(qstnDisplay);
+  clearHTML();
+  console.log(qstnDisplay);
+  var initials = window.prompt(
+    "Your score was " +
+      points +
+      ". Please enter your initials to save your score!"
+  );
+
+  myScore.initials.push(initials);
+  myScore.score.push(points);
+  localStorage.setItem("scores", JSON.stringify(myScore));
+
+  var myScoreDisplay = document.createElement("div");
+  myScoreDisplay.className = "score-list-container";
+
+  var myScoreOrder = document.createElement("ol");
+  myScoreOrder.className = "ordered-list";
+
+  for (i = 0; i < myScore.score.length; i++) {
+    var myScoreList = document.createElement("li");
+    myScoreList.className = "score-list";
+    myScoreList.textContent = myScore.initials[i] + "--" + myScore.score[i];
+    myScoreOrder.prepend(myScoreList);
+  }
+  myScoreDisplay.appendChild(myScoreOrder);
+  qstnDisplay.appendChild(myScoreDisplay);
+};
+
+var loadScore = function () {
+  var savedScore = localStorage.getItem("scores");
+  if (!savedScore) {
+    return false;
+  }
+  myScore = JSON.parse(savedScore);
+};
+
+loadScore();
